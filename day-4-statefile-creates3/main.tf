6b70b4c06ed0135e15a5f0d498dc3bbb459983a1
Anurag <anuragurmaliya911@gmail.com>
2024-05-16 13:42:50 +0530

    new updation

--- day-4-statefile-creates3/main.tf
+++ day-4-statefile-creates3/main.tf
@@ -12,7 +12,16 @@ resource "aws_s3_bucket_versioning" "example" {
 
   
 }
-
-resource "dynamo_db" "name" {
   
+  #DynamoDB creation for locking process
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
--- day4-remote-statefile-configuration/main.tf
+++ day4-remote-statefile-configuration/main.tf
@@ -2,13 +2,15 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "ere" {
+resource "aws_instance" "remote" {
     ami = "ami-0cc9838aa7ab1dce7"
     instance_type = "t2.micro"
     key_name = "anuragkey3"
 
     tags = {
-      Name = "newdev"
+      Name = "anurag1"
+
+   
     }
   
 }
